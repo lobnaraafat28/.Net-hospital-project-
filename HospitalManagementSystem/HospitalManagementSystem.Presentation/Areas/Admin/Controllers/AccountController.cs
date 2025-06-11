87b539231da823ac7983018394d8f07e1ab8b6e6
lobna21 <lobna21@outlook.com>
2025-06-11 17:12:48 +0200

    Login module

--- HospitalManagementSystem/HospitalManagementSystem.Presentation/Areas/Admin/Controllers/AccountController.cs
+++ HospitalManagementSystem/HospitalManagementSystem.Presentation/Areas/Admin/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using HospitalManagementSystem.Presentation.Areas.Admin.Models;
+﻿using HospitalManagementSystem.Models.Users;
+using HospitalManagementSystem.Presentation.Areas.Admin.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalManagementSystem.Presentation.Areas.Admin.Controllers
@@ -6,14 +8,38 @@ namespace HospitalManagementSystem.Presentation.Areas.Admin.Controllers
     [Area("Admin")]
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager,SignInManager<User> signInManager)
+        {
+           _userManager = userManager;
+           _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Login(LoginVM loginVM)
+        public async Task<IActionResult> Login(LoginVM loginVM)
         {
-            return View();
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            var result = await _signInManager.PasswordSignInAsync(
+                loginVM.UserName,
+                loginVM.Password,
+                loginVM.RememberMe,
+                lockoutOnFailure: false
+            );
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty,"The username or password is wrong!");
+            return View(loginVM);
         }
     }
 }
--- HospitalManagementSystem/HospitalManagementSystem.Presentation/Areas/Admin/Models/LoginVM.cs
+++ HospitalManagementSystem/HospitalManagementSystem.Presentation/Areas/Admin/Models/LoginVM.cs
@@ -1,8 +1,17 @@
-﻿namespace HospitalManagementSystem.Presentation.Areas.Admin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalManagementSystem.Presentation.Areas.Admin.Models
 {
     public class LoginVM
     {
+        [Required]
+        [EmailAddress]
         public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
